e620f94bd9f87255914ca5aaeca0a059f6a07a03
Arne Van Raepenbusch <arne.vanraepenbusch@jstack.eu>
2019-05-28 15:24:06 +0200

    Don't reinitialize api when closing window

--- src/electron/main.js
+++ src/electron/main.js
@@ -2,29 +2,29 @@ require('app-module-path').addPath(`${__dirname}`);
 
 global.R = require('ramda');
 
+const Api = require('express')();
+const HTTP = require('http').Server(Api);
+const IO = require('socket.io')(HTTP);
+
+const Cors = require('cors');
+
+Api.use(Cors());
+
+const Game = require('game/Game');
+
+require('communication/routes/GameRoutes')(Api);
+require('communication/routes/PlayerRoutes')(Api);
+
+HTTP.listen(9001, () => {
+    console.log('listening on *:9001');
+    Game.create();
+    Game.start();
+});
+
+require('communication/sockets/Connector').initialize(IO);
+
 module.exports = (App, window) => {
 
     const ipc = require('electron').ipcMain;
-
-    const Api = require('express')();
-    const HTTP = require('http').Server(Api);
-    const IO = require('socket.io')(HTTP);
-
-    const Cors = require('cors');
-
-    Api.use(Cors());
-
-    const Game = require('game/Game');
-
-    require('communication/routes/GameRoutes')(Api);
-    require('communication/routes/PlayerRoutes')(Api);
-
-    HTTP.listen(9001, () => {
-        console.log('listening on *:9001');
-        Game.create();
-        Game.start();
-    });
-
-    require('communication/sockets/Connector').initialize(IO);
 
 };
