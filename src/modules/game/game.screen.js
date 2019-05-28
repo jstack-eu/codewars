2f1491074052832665229b492e9dff2c399386b3
tombuyse <tom.buyse5@gmail.com>
2019-05-28 15:28:06 +0200

    cleanup bullets

--- src/modules/game/game.screen.js
+++ src/modules/game/game.screen.js
@@ -59,6 +59,8 @@ const GameScreen = observer(() => {
     const drawBullets = () => {
         const [playerNode, bulletsNode] = nodes;
 
+        playerNode.getContext('2d').clearRect(0, 0, 1600, 960);
+
         gameStore.bullets.map(bullet => drawBullet({
             context: bulletsNode.getContext('2d'),
             x: bullet.x,
